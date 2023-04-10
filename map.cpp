01e1f3e307c583fc2ddf7862f1b861e882643314
Ilyas qabbal <ilyas.qabbal.2020@usms.ac.ma>
2023-04-10 09:59:50 +0000

    g

--- map.cpp
+++ map.cpp
@@ -9,3 +9,30 @@ int main()
     for(auto mm : m)
         cout << mm.first << "-" << mm.second << endl;
 }
+
+
+
+
+
+#include <iostream>
+#include <map>
+
+int main() {
+    std::map<int, std::string> myMap;
+
+    myMap[1] = "apple";
+    myMap[2] = "banana";
+    myMap[3] = "cherry";
+    myMap[4] = "date";
+    myMap[5] = "elderberry";
+
+    // Find the first element with a key >= 3
+    auto it = myMap.lower_bound(3);
+
+    // Print out all elements from the lower bound to the end
+    for (; it != myMap.end(); ++it) {
+        std::cout << it->first << ": " << it->second << std::endl;
+    }
+
+    return 0;
+}
